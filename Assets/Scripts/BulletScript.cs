10ede7e9019fb616083137990c29f0b5dfe635b2
MattFairchild <matt_fv1@hotmail.com>
2015-05-22 22:57:07 +0200

    Small correction to BulletScript to have them destroyed after 10 secs. if they just fly out of screen

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -5,12 +5,23 @@ public class BulletScript : MonoBehaviour {
 
     public Rigidbody rb;
     public float speed = 25.0f;
+    public float lifetime = 10.0f;
 
 	// Use this for initialization
 	void Start () {
         this.transform.rotation = Quaternion.AngleAxis(90.0f, this.transform.right);
         rb.velocity = this.transform.up * speed;
 	}
+
+    public void Update()
+    {
+        lifetime -= Time.deltaTime;
+
+        if (lifetime < 0.0f)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
 
     public void SetVelocity(Vector3 vel)
     {
