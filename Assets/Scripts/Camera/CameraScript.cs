37e6769b171e88e65f86fae7d6529db3bbd6a46f
Daniel <daniel.fedh@gmail.com>
2015-06-28 01:22:54 +0200

    complete UI revision for Standard scene minor UI revision for Menu scene

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -1,74 +1,78 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraScript : MonoBehaviour {
-	
-	public float xBounds = 30.0f;
-	public float zBounds = 20.0f;
-	public float yDistance = 20.0f;
-	
-	private Transform player;
-	private Camera cam;
+public class CameraScript : MonoBehaviour
+{
 
-	void OnDrawGizmos () {
-		Color regColor = Gizmos.color;
-		Gizmos.color = Color.blue;
+    public float xBounds = 30.0f;
+    public float zBounds = 20.0f;
+    public float yDistance = 20.0f;
 
-		//Gizmos.DrawLine();
-		Gizmos.DrawWireCube(Vector3.zero + Vector3.up * yDistance, 2 * new Vector3(xBounds, 0, zBounds));
+    private Transform player;
+    private Camera cam;
 
-		Gizmos.color = regColor;
-	}
+    void OnDrawGizmos()
+    {
+        Color regColor = Gizmos.color;
+        Gizmos.color = Color.blue;
 
-	// Use this for initialization
-	void Start () {
-		player = GameObject.FindGameObjectWithTag("Player").transform;
-		this.transform.position = new Vector3(player.position.x, yDistance, player.position.z);
-		cam = GetComponent<Camera>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		float correctX, correctZ, correctY = 20.0f;
-		bool correct = false;
-		this.transform.position = new Vector3(player.position.x, this.transform.position.y, player.position.z);
-		
-		if (transform.position.x < -xBounds)
-		{
-			correctX = -xBounds;
-			correct = true;
-		}
-		else if (transform.position.x > xBounds)
-		{
-			correctX = xBounds;
-			correct = true;
-		}
-		else 
-		{
-			correctX = transform.position.x;
-		}
-		
-		
-		if (transform.position.z < -zBounds)
-		{
-			correctZ = -zBounds;
-			correct = true;
-		}
-		else if (transform.position.z > zBounds)
-		{
-			correctZ = zBounds;
-			correct = true;
-		}
-		else
-		{
-			correctZ = transform.position.z;
-		}
-		
-		
-		if (correct)
-		{
-			this.transform.position = new Vector3(correctX, correctY, correctZ);
-		}
-		
-	}
+        //Gizmos.DrawLine();
+        Gizmos.DrawWireCube(Vector3.zero + Vector3.up * yDistance, 2 * new Vector3(xBounds, 0, zBounds));
+
+        Gizmos.color = regColor;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        this.transform.position = new Vector3(player.position.x, yDistance, player.position.z - 2);
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float correctX, correctZ, correctY = 20.0f;
+        bool correct = false;
+        this.transform.position = new Vector3(player.position.x, this.transform.position.y, player.position.z - 2);
+
+        if (transform.position.x < -xBounds)
+        {
+            correctX = -xBounds;
+            correct = true;
+        }
+        else if (transform.position.x > xBounds)
+        {
+            correctX = xBounds;
+            correct = true;
+        }
+        else
+        {
+            correctX = transform.position.x;
+        }
+
+
+        if (transform.position.z < -zBounds)
+        {
+            correctZ = -zBounds;
+            correct = true;
+        }
+        else if (transform.position.z > zBounds)
+        {
+            correctZ = zBounds;
+            correct = true;
+        }
+        else
+        {
+            correctZ = transform.position.z;
+        }
+
+
+        if (correct)
+        {
+            this.transform.position = new Vector3(correctX, correctY, correctZ);
+        }
+
+    }
 }
--- Assets/Scripts/GUI/GUI.cs
+++ Assets/Scripts/GUI/GUI.cs
@@ -2,67 +2,89 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class GUI : MonoBehaviour {
+public class GUI : MonoBehaviour
+{
 
-	[SerializeField]protected RectTransform scoreGUIParent;
-	[SerializeField]protected Text score;
-	[SerializeField]protected RectTransform timerGUIParent;
-	[SerializeField]protected Text timer;
-	[SerializeField]protected Image timerCircle;
-	
-	[SerializeField]protected RectTransform playerGUIParent;
-	[SerializeField]protected Image playerHealth;
-	[SerializeField]protected Image playerShield;
-	[SerializeField]protected Image playerShards;
-	[SerializeField]protected Slider playerSpeed;
+    [SerializeField]
+    protected RectTransform scoreGUIParent;
+    [SerializeField]
+    protected Text score;
+    [SerializeField]
+    protected RectTransform timerGUIParent;
+    [SerializeField]
+    protected Text timer;
+    [SerializeField]
+    protected Image timerCircle;
 
-	[SerializeField]protected RectTransform baseGUIParent;
-	[SerializeField]protected Image baseHealth;
-	[SerializeField]protected Image baseShield;
+    [SerializeField]
+    protected RectTransform playerGUIParent;
+    [SerializeField]
+    protected Image playerHealth;
+    [SerializeField]
+    protected Image playerShield;
+    [SerializeField]
+    protected Image playerShards;
+    [SerializeField]
+    protected Slider playerSpeed;
 
-	[SerializeField]protected RectTransform bossGUIParent;
-	[SerializeField]protected Image bossHealth;
-	[SerializeField]protected Image bossShield;
+    [SerializeField]
+    protected RectTransform baseGUIParent;
+    [SerializeField]
+    protected Image baseHealth;
+    [SerializeField]
+    protected Image baseShield;
 
-	[SerializeField]protected Image pauseOverlay;
-	[SerializeField]protected RectTransform pauseImage;
+    [SerializeField]
+    protected RectTransform bossGUIParent;
+    [SerializeField]
+    protected Image bossHealth;
+    [SerializeField]
+    protected Image bossShield;
 
-	protected Player player;
-	protected SpaceStationScript spaceStation;
-	protected Image playerHealthOverlay;
-	protected Image baseHealthOverlay;
+    [SerializeField]
+    protected Image pauseOverlay;
+    [SerializeField]
+    protected RectTransform pauseImage;
 
-	protected bool lastPaused = false;
+    protected Player player;
+    protected SpaceStationScript spaceStation;
+    protected Image playerHealthOverlay;
+    protected Image baseHealthOverlay;
 
-	// Use this for initialization
-	void Start () {
-		player = GameManager.player;
-		spaceStation = GameManager.spaceStation;
-		playerHealthOverlay = player.healthBarOverlay;
-		baseHealthOverlay = spaceStation.healthBarOverlay;
-		//input = player.input;
-		bossGUIParent.gameObject.SetActive(false);
-		pauseOverlay.CrossFadeAlpha(0.0f, 0.0f, true);
-		pauseImage.gameObject.SetActive(false);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		score.text = "<size=50><i>Score</i></size>\n" + GameManager.score;
-		float timeLeft = GameManager.endTime - Time.time;
-		timer.text = "<size=50><i>Time</i></size>\n" + (int)timeLeft;
-		timerCircle.fillAmount = timeLeft / GameManager.gameDuration;
+    protected bool lastPaused = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameManager.player;
+        spaceStation = GameManager.spaceStation;
+        playerHealthOverlay = player.healthBarOverlay;
+        baseHealthOverlay = spaceStation.healthBarOverlay;
+        //input = player.input;
+        bossGUIParent.gameObject.SetActive(false);
+        pauseOverlay.CrossFadeAlpha(0.0f, 0.0f, true);
+        pauseImage.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = GameManager.score + "\n<size=50><i>Score</i></size>";
+        float timeLeft = GameManager.endTime - Time.time;
+        //timer.text = "<size=50><i>Time</i></size>\n" + (int)timeLeft;
+        timerCircle.fillAmount = timeLeft / GameManager.gameDuration;
         playerShield.fillAmount = GameManager.boostTimer;
-		playerShards.fillAmount = 1.0f*GameManager.curResources/GameManager.maxResources;
-		playerSpeed.value = player.speed; //input.getSpeed() * (input.placingTurret() ? 0.5f : 1);
-		playerHealth.fillAmount = playerHealthOverlay.fillAmount = player.percentOfHealth;
-		baseHealth.fillAmount = baseHealthOverlay.fillAmount = spaceStation.percentOfHealth;
-		bool paused = GameManager.gamePaused;
-		if(paused != lastPaused){
-			lastPaused = paused;
-			float targetAlpha = paused ? 1.0f : 0.0f;
-			pauseOverlay.CrossFadeAlpha(targetAlpha, 1.0f, true);
-			pauseImage.gameObject.SetActive(paused);
-		}
-	}
+        playerShards.fillAmount = 1.0f * GameManager.curResources / GameManager.maxResources;
+        playerSpeed.value = player.speed; //input.getSpeed() * (input.placingTurret() ? 0.5f : 1);
+        playerHealth.fillAmount = playerHealthOverlay.fillAmount = player.percentOfHealth;
+        baseHealth.fillAmount = baseHealthOverlay.fillAmount = spaceStation.percentOfHealth;
+        bool paused = GameManager.gamePaused;
+        if (paused != lastPaused)
+        {
+            lastPaused = paused;
+            float targetAlpha = paused ? 1.0f : 0.0f;
+            pauseOverlay.CrossFadeAlpha(targetAlpha, 1.0f, true);
+            pauseImage.gameObject.SetActive(paused);
+        }
+    }
 }
