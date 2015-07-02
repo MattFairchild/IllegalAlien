5269c293553c574ea6bc79d975e89ccbf2830f96
Daniel <daniel.fedh@gmail.com>
2015-07-02 21:22:19 +0200

    Updated:   Player.prefab   Destroyer.prefab   Interceptor.prefab with symmetrical 180° health-bar
    Fixed missing material for TurretLVL3

---
+++ Assets/Scripts/PropergateFillAmountToChildren.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PropergateFillAmountToChildren : MonoBehaviour
+{
+
+    private Image[] Images;
+
+    void Start()
+    {
+        Images = GetComponentsInChildren<Image>();
+    }
+
+    void Update()
+    {
+        for (int i = 1; i < Images.Length; i++)
+        {
+            Images[i].fillAmount = GetComponent<Image>().fillAmount;
+        }
+    }
+}
